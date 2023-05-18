cb3324d36ad444cf6673a947e10e301fbda550bf
Deep Panchal <deep302001@gmail.com>
2023-05-18 11:26:09 -0500

    feat(vim): update astronvim config, add rainbow_csv plugin config

--- dotfiles/config/nvim/lua/user/plugins/user.lua
+++ dotfiles/config/nvim/lua/user/plugins/user.lua
@@ -49,4 +49,23 @@ return {
     event = "BufEnter *justfile,*Justfile",
     config = function() require("tree-sitter-just").setup {} end,
   },
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
 }
